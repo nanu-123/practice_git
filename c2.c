e8a7f183910a71818f9268d38d2c9ddba916a72c
nanu-123 <nancynadeson@gmail.com>
2024-07-20 20:09:52 +0530

    second commit through UI

--- c2.c
+++ c2.c
@@ -13,7 +13,7 @@ int main() {
     sum = num1 + num2;
 
     // Print the result
-    printf("Sum of %d and %d is %d\n", num1, num2, sum);
+    // printf("Sum of %d and %d is %d\n", num1, num2, sum);
 
     return 0;
 }
